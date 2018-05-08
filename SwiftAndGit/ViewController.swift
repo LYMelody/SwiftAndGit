4651872909c337deece1f48ec9bc46610ed95cd2
zhouhuiping <zhouhuiping@souche.com>
2018-05-08 20:45:09 +0800

    添加个打电话按钮

--- SwiftAndGit/ViewController.swift
+++ SwiftAndGit/ViewController.swift
@@ -12,6 +12,17 @@ class ViewController: UIViewController {
 
     
     let mainViewController = UIViewController();
+    
+    var phoneButton: UIButton = {
+        let button = UIButton()
+        button.frame = CGRect(x: 0, y: 0, width: 60, height: 30)
+        button.setTitle("打电话", for: .normal)
+        button.backgroundColor = UIColor.orange
+        button.addTarget(self, action: #selector(TEl), for: .touchUpInside)
+        return button
+    }()
+    
+    
     override func viewDidLoad() {
         
         super.viewDidLoad()
@@ -37,8 +48,25 @@ class ViewController: UIViewController {
 
         print("remote branch")
     
+        view.addSubview(phoneButton)
+        phoneButton.center = view.center
+        
     }
 
+    @objc func TEl() {
+        UIApplication.shared.open(NSURL(string: "tel://18868825142")! as URL, options: [:]) { (success) in
+            if success == true {
+                print("open:\(success)")
+            } else {
+                print("open:\(success)")
+            }
+        }
+        
+        let flag =  UIApplication.shared.canOpenURL(NSURL(string: "tel://18868825142")! as URL)
+        
+        print(flag)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
